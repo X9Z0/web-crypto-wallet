0350a7fbf0fc7a0aed74dd28bdb872e6741db1c0
X9Z0 <x9z0cofd@gmail.com>
2024-09-09 00:28:27 +0530

    key fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,4 @@
-import { Button } from "@/components/ui/button";
-import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
 import MainPage from "@/components/mainPage";
-import MemoWord from "@/components/ui/memoword";
-import ThirdPage from "@/components/thirdPage";
-import FourthPage from "@/components/fourthPage";
 export default function Home() {
   return (
     <main>
